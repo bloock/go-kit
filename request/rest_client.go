c0b5ed5df9d03ee29ba311796e72d797a7694def
edutomesco <eduard.escoruela@gmail.com>
2022-01-03 13:47:03 +0100

    EIT-1773 | add post module

--- request/rest_client.go
+++ request/rest_client.go
@@ -1,9 +1,16 @@
 package request
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+)
 
 type RestClient struct{}
 
 func (r RestClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
+}
+
+func (r RestClient) Post(url, contentType string, body []byte) (*http.Response, error) {
+	return http.Post(url, contentType, bytes.NewBuffer(body))
 }
