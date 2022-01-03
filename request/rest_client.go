33f518980b40615906adfd821fb8b9773b3221aa
Marc Baqué <marcbaque96@gmail.com>
2022-01-03 14:08:19 +0100

    Added response unmarshall to post http request

--- request/rest_client.go
+++ request/rest_client.go
@@ -14,26 +14,31 @@ func (r RestClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
 }
 
-func (r RestClient) Post(url string, body interface{}) ([]byte, error) {
+func (r RestClient) Post(url string, body interface{}, response interface{}) error {
 	b, err := json.Marshal(body)
 	if err != nil {
-		return []byte{}, err
+		return err
 	}
 
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(b))
 	if err != nil {
-		return []byte{}, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	respByte, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return []byte{}, err
+		return err
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return []byte{}, errors.New("invalid status code")
+		return errors.New("invalid status code")
 	}
 
-	return respByte, nil
+	err = json.Unmarshal(respByte, &response)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
