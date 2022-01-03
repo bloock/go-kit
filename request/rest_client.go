4e5a73fc547a4821efedc8288a55483e3e7aada5
edutomesco <eduard.escoruela@gmail.com>
2022-01-03 14:04:49 +0100

    EIT-1773 | adapting post http

--- request/rest_client.go
+++ request/rest_client.go
@@ -2,6 +2,9 @@ package request
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -11,6 +14,26 @@ func (r RestClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
 }
 
-func (r RestClient) Post(url, contentType string, body []byte) (*http.Response, error) {
-	return http.Post(url, contentType, bytes.NewBuffer(body))
+func (r RestClient) Post(url string, body interface{}) ([]byte, error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+
+	respByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return []byte{}, errors.New("invalid status code")
+	}
+
+	return respByte, nil
 }
