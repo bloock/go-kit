03eee16bad0d102904bc090eb8497855c0ae88ab
edutomesco <eduard.escoruela@gmail.com>
2022-01-17 07:59:04 +0100

    EIT-1782 | modify GET to be equal as POST

--- request/rest_client.go
+++ request/rest_client.go
@@ -10,8 +10,28 @@ import (
 
 type RestClient struct{}
 
-func (r RestClient) Get(url string) (*http.Response, error) {
-	return http.Get(url)
+func (r RestClient) Get(url string, response interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return errors.New("invalid status code")
+	}
+
+	err = json.Unmarshal(respByte, &response)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r RestClient) Post(url string, body interface{}, response interface{}) error {
