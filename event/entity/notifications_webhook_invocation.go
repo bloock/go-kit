1d58b62b47a335248795185069de3376cf7fa6a8
edutomesco <eduard.escoruela@gmail.com>
2022-04-19 10:06:10 +0200

    EIT-2113 | change payload from string to interface{}

--- event/entity/notifications_webhook_invocation.go
+++ event/entity/notifications_webhook_invocation.go
@@ -6,13 +6,13 @@ type NotificationsWebhookInvocation struct {
 	Id         string    `json:"id"`
 	WebhookId  string    `json:"webhook_id"`
 	WType      string    `json:"w_type"`
-	Payload    string    `json:"payload"`
+	Payload    interface{}    `json:"payload"`
 	RequestId  string    `json:"request_id"`
 	Timestamp  time.Time `json:"timestamp"`
 	Expiration int       `json:"expiration"`
 }
 
-func NewWebhookInvocationEventEntity(id, wId, wType, payload, rId string, timestamp time.Time, expiration int) NotificationsWebhookInvocation {
+func NewWebhookInvocationEventEntity(id, wId, wType string, payload interface{}, rId string, timestamp time.Time, expiration int) NotificationsWebhookInvocation {
 	return NotificationsWebhookInvocation{
 		Id:         id,
 		WebhookId:  wId,
