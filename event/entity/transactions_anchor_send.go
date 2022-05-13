244d3d084fbefae08d8f403606f1687a98661a6b
edutomesco <eduard.escoruela@gmail.com>
2022-05-13 08:35:45 +0200

    EIT-2158 | add expiration to send anchor type

--- event/entity/transactions_anchor_send.go
+++ event/entity/transactions_anchor_send.go
@@ -1,13 +1,15 @@
 package event_entity
 
 type TransactionsAnchorSend struct {
-	Anchor int32  `json:"anchor"`
-	State  string `json:"state"`
+	Anchor     int32  `json:"anchor"`
+	State      string `json:"state"`
+	Expiration int    `json:"expiration"`
 }
 
-func NewTransactionsAnchorSend(anchor int32, state string) TransactionsAnchorSend {
+func NewTransactionsAnchorSend(anchor int32, state string, expiration int) TransactionsAnchorSend {
 	return TransactionsAnchorSend{
-		Anchor: anchor,
-		State:  state,
+		Anchor:     anchor,
+		State:      state,
+		Expiration: expiration,
 	}
 }
