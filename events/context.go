55836e906badb1d785577417e9b9cec4e56266e0
edutomesco <eduard.escoruela@gmail.com>
2022-06-30 17:46:51 +0200

    EIT-2302 | add new read body request and response structure

--- events/context.go
+++ events/context.go
@@ -2,7 +2,6 @@ package events
 
 import (
 	"github.com/gin-gonic/gin"
-	"io"
 )
 
 type ResponseContext struct {
@@ -16,19 +15,14 @@ type ResponseContext struct {
 	UserID       string
 }
 
-func NewResponseContext(c *gin.Context, w wrappedWriter, typ string) (*ResponseContext, error) {
-
-	requestBody, err := io.ReadAll(c.Request.Body)
-	if err != nil {
-		return &ResponseContext{}, err
-	}
+func NewResponseContext(c *gin.Context, w wrappedWriter, typ, requestBody string) (*ResponseContext, error) {
 
 	return &ResponseContext{
 		Type:         typ,
 		Status:       w.Status(),
 		Path:         c.Request.URL.Path,
 		RequestID:    c.Request.Header.Get("X-Request-ID"),
-		RequestBody:  string(requestBody),
+		RequestBody:  requestBody,
 		ResponseBody: w.body.String(),
 		IP:           c.ClientIP(),
 		UserID:       c.Request.Header.Get("x-user-id"),
--- events/events_middleware.go
+++ events/events_middleware.go
@@ -6,30 +6,41 @@ import (
 	"github.com/bloock/go-kit/request"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"io"
 	"log"
 )
 
 type MiddlewareEvent struct {
 	logger zerolog.Logger
+	endpointConfig EndpointConfig
 }
 
-func NewMiddlewareEvent(l zerolog.Logger) MiddlewareEvent {
+func NewMiddlewareEvent(l zerolog.Logger, protocol, host string, port int, path string) MiddlewareEvent {
 	return MiddlewareEvent{
 		logger: l,
+		endpointConfig: EndpointConfig{protocol, host, port, path},
 	}
 }
 
 func (m MiddlewareEvent) MiddlewareEvents(typ string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		requestBody, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			m.logger.Error().Msg(err.Error())
+		}
+		c.Request.Body.Close()
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+
 		writer := c.Writer
 		rw := wrappedWriter{ResponseWriter: c.Writer}
 		c.Writer = &rw
 		c.Next()
 		c.Writer = writer
 
-		body, err := NewResponseContext(c, rw, typ)
+		body, err := NewResponseContext(c, rw, typ, string(requestBody))
 
-		url := fmt.Sprintf("%s://%s/%s", "https", "api.bloock.dev", "events/v1/activities")
+		url := fmt.Sprintf("%s://%s:%d%s", m.endpointConfig.Protocol, m.endpointConfig.Host,
+			m.endpointConfig.Port, m.endpointConfig.Path)
 
 		log.Printf("Info --> %+v", body)
 		err = request.RestClient{}.Post(url, body, nil)
@@ -39,6 +50,13 @@ func (m MiddlewareEvent) MiddlewareEvents(typ string) gin.HandlerFunc {
 			return
 		}
 	}
+}
+
+type EndpointConfig struct {
+	Protocol string
+	Host     string
+	Port     int
+	Path     string
 }
 
 type wrappedWriter struct {
