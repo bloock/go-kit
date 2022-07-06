b7b22f97aef8ef8f8bd5c92d39f483f6aa6f2f12
edutomesco <eduard.escoruela@gmail.com>
2022-07-06 10:57:17 +0200

    hotfix | add test vatiables for type event

--- events/types.go
+++ events/types.go
@@ -1,6 +1,9 @@
 package events
 
+import "fmt"
+
 type EventType int32
+type EventTypeTest int32
 
 const (
 	GetAnchor EventType = iota
@@ -8,14 +11,38 @@ const (
 	NewMessages
 )
 
+const(
+	GetAnchorTest EventTypeTest = iota
+	GetProofTest
+	NewMessagesTest
+)
+
+const(
+	CorePrefix     = "core"
+	CoreTestPrefix = "core-test"
+)
+
 func (e EventType) Str() string {
 	switch e {
 	case GetAnchor:
-		return "get_anchor"
+		return fmt.Sprintf("%s.%s", CorePrefix, "get_anchor")
 	case GetProof:
-		return "get_proof"
+		return fmt.Sprintf("%s.%s", CorePrefix, "get_proof")
 	case NewMessages:
-		return "new_messages"
+		return fmt.Sprintf("%s.%s", CorePrefix, "new_messaages")
+	default:
+		return ""
+	}
+}
+
+func (e EventTypeTest) Str() string {
+	switch e {
+	case GetAnchorTest:
+		return fmt.Sprintf("%s.%s", CoreTestPrefix, "get_anchor")
+	case GetProofTest:
+		return fmt.Sprintf("%s.%s", CoreTestPrefix, "get_proof")
+	case NewMessagesTest:
+		return fmt.Sprintf("%s.%s", CoreTestPrefix, "new_messaages")
 	default:
 		return ""
 	}
