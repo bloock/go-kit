55a1ec5266ecea6b58b557d498273c4c8c900189
Eduard Tomas <61798159+edutomesco@users.noreply.github.com>
2022-07-12 14:24:55 +0200

    EIT-hot-fix | add unimplemented methods on http request interface (#41)

--- request/http_request.go
+++ request/http_request.go
@@ -1,9 +1,9 @@
 package request
 
-//go:generate mockgen -package=mocks -source=request/http_request.go -destination mocks/mock_http_request.go
+//go:generate mockgen -package=mocks -source=request/http_request.go -destination mocks/request/mock_http_request.go
 type HttpRequest interface {
 	Get(url string, response interface{}) error
-	Post(url string, body interface{}, response interface{}) error
-	PostWithHeaders(url string, body, response interface{}, headers map[string]string) error
+	Post(url string, body interface{}, response interface{}, contentType string) error
+	PostWithHeaders(url string, body, response interface{}, headers map[string]string, contentType string) error
 	Delete(url string, body interface{}, response interface{}, headers map[string]string) error
 }
