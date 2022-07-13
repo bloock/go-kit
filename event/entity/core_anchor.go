65f65ec6c385e438273523c0f5c87a1dcda2484d
edutomesco <eduard.escoruela@gmail.com>
2022-07-13 13:50:23 +0200

    EIT-2369 | adapt CoreAnchor entity event

--- event/entity/core_anchor.go
+++ event/entity/core_anchor.go
@@ -1,20 +1,20 @@
 package event_entity
 
 type CoreAnchor struct {
-	Id           int                 `json:"id"`
-	Root         string              `json:"root"`
-	Status       string              `json:"status"`
-	Test         bool                `json:"test"`
-	MessageCount int64               `json:"message_count"`
-	CreatedAt    int64               `json:"created_at"`
-	Networks     []CoreAnchorNetwork `json:"networks"`
+	Id           int               `json:"id"`
+	Root         string            `json:"root"`
+	Finalized    bool              `json:"finalized"`
+	Test         bool              `json:"test"`
+	MessageCount int64             `json:"message_count"`
+	CreatedAt    int64             `json:"created_at"`
+	Networks     CoreAnchorNetwork `json:"networks"`
 }
 
-func NewCoreAnchorEventEntity(id int, root, status string, test bool, messageCount, createdAt int64, networks []CoreAnchorNetwork) CoreAnchor {
+func NewCoreAnchorEventEntity(id int, root string, finalized, test bool, messageCount, createdAt int64, networks CoreAnchorNetwork) CoreAnchor {
 	return CoreAnchor{
 		Id:           id,
 		Root:         root,
-		Status:       status,
+		Finalized:    finalized,
 		Test:         test,
 		MessageCount: messageCount,
 		CreatedAt:    createdAt,
