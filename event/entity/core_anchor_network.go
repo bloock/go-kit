1a4d20194600e585f4bcf9134767859f68f2cfd5
edutomesco <eduard.escoruela@gmail.com>
2022-07-29 14:05:31 +0200

    master | hotfix

--- event/entity/core_anchor_network.go
+++ event/entity/core_anchor_network.go
@@ -1,21 +1,10 @@
 package event_entity
 
 type CoreAnchorNetwork struct {
-	Root      string `json:"root"`
+	AnchorId  int    `json:"anchor_id"`
 	Name      string `json:"name"`
 	Status    string `json:"status"`
 	Test      bool   `json:"test"`
 	TxHash    string `json:"tx_hash"`
 	CreatedAt int64  `json:"created_at"`
-}
-
-func NewCoreAnchorNetworkEventEntity(root, name, status string, test bool, txHash string, createdAt int64) CoreAnchorNetwork {
-	return CoreAnchorNetwork{
-		Root:      root,
-		Name:      name,
-		Status:    status,
-		Test:      test,
-		TxHash:    txHash,
-		CreatedAt: createdAt,
-	}
 }
