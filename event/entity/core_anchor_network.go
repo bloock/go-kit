08c514baafbeb745cd06215910cf278c7dbeebf2
Eduard Tomas <61798159+edutomesco@users.noreply.github.com>
2022-07-29 12:08:44 +0200

    EIT-2404 | add anchor network unify to new webhook information (#47)

--- event/entity/core_anchor_network.go
+++ event/entity/core_anchor_network.go
@@ -1,10 +1,21 @@
 package event_entity
 
 type CoreAnchorNetwork struct {
-	AnchorId  int    `json:"anchor_id"`
+	Root      string `json:"root"`
 	Name      string `json:"name"`
 	Status    string `json:"status"`
 	Test      bool   `json:"test"`
 	TxHash    string `json:"tx_hash"`
 	CreatedAt int64  `json:"created_at"`
+}
+
+func NewCoreAnchorNetworkEventEntity(root, name, status string, test bool, txHash string, createdAt int64) CoreAnchorNetwork {
+	return CoreAnchorNetwork{
+		Root:      root,
+		Name:      name,
+		Status:    status,
+		Test:      test,
+		TxHash:    txHash,
+		CreatedAt: createdAt,
+	}
 }
