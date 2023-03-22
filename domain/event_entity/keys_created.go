4ed1e435e0ed9f277f91746f8272cb354856b0e9
edutomesco <eduard.escoruela@gmail.com>
2023-03-22 08:49:12 +0100

    master | delete keyID from entity event report

--- domain/event_entity/keys_created.go
+++ domain/event_entity/keys_created.go
@@ -2,14 +2,12 @@ package event_entity
 
 type KeyCreated struct {
 	IssuerID      string `json:"issuer_id"`
-	KeyID         string `json:"key_id"`
 	KeyProtection int    `json:"key_protection"`
 }
 
-func NewKeyCreated(issuerID string, keyID string, keyProtection int) KeyCreated {
+func NewKeyCreated(issuerID string, keyProtection int) KeyCreated {
 	return KeyCreated{
 		IssuerID:      issuerID,
-		KeyID:         keyID,
 		KeyProtection: keyProtection,
 	}
 }
