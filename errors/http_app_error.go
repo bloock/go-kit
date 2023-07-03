7c883782c15206f4fb37d04c435e063cc8e95302
edutomesco <eduard.escoruela@gmail.com>
2023-07-03 11:06:03 +0200

    master | delete log with observability because do not do tracer

--- errors/http_app_error.go
+++ errors/http_app_error.go
@@ -1,9 +1,7 @@
 package errors
 
 import (
-	"context"
 	"fmt"
-	"github.com/bloock/go-kit/observability"
 )
 
 type HttpAppError struct {
@@ -17,9 +15,4 @@ func NewHttpAppError(code int, message string) HttpAppError {
 
 func (e HttpAppError) Error() string {
 	return fmt.Sprintf("errors: %s. code: %d.", e.Message, e.Code)
-}
-
-func (e HttpAppError) LogErrorTraceability(ctx context.Context, logger observability.Logger) HttpAppError {
-	logger.Info(ctx).Err(e).Msg("")
-	return e
 }
