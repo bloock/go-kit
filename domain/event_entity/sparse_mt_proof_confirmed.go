a9a2a4b898242054dc3a94800553b96aab477717
edutomesco <eduard.escoruela@gmail.com>
2023-08-18 09:10:31 +0100

    master | change integrity and sparse mt proofs events body to proof string because it's gonna come the proof encoded in string format

--- domain/event_entity/sparse_mt_proof_confirmed.go
+++ domain/event_entity/sparse_mt_proof_confirmed.go
@@ -1,13 +1,13 @@
 package event_entity
 
 type SparseMerkleTreeProofConfirmed struct {
-	CredentialID string      `json:"credential_id"`
-	Proof        interface{} `json:"proof"`
+	CredentialID string `json:"credential_id"`
+	Proof        string `json:"proof"`
 }
 
-func NewSparseMerkleTreeProofConfirmed(credentialID string, proof interface{}) SparseMerkleTreeProofConfirmed {
+func NewSparseMerkleTreeProofConfirmed(credentialID string, proofEncoded string) SparseMerkleTreeProofConfirmed {
 	return SparseMerkleTreeProofConfirmed{
 		CredentialID: credentialID,
-		Proof:        proof,
+		Proof:        proofEncoded,
 	}
 }
