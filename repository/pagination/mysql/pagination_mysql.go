62eff7d1340f1daacf9d94bd3b4789d52894d690
edutomesco <eduard.escoruela@gmail.com>
2024-01-04 09:53:25 +0100

    master | hot-fix | delete get pagination pgx

--- repository/pagination/mysql/pagination_mysql.go
+++ repository/pagination/mysql/pagination_mysql.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/bloock/go-kit/repository/pagination"
 	"github.com/huandu/go-sqlbuilder"
-	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type paginationResponse struct {
@@ -25,29 +24,6 @@ func GetPagination(ctx context.Context, db *sql.DB, sb *sqlbuilder.SelectBuilder
 	defer cancel()
 
 	rows, err := db.QueryContext(ctxTimeout, query, args...)
-	if err != nil {
-		return pagination.Pagination{}, err
-	}
-	defer rows.Close()
-
-	rows.Next()
-	var pr paginationResponse
-	rows.Scan(paginationSQLStruct.Addr(&pr)...)
-
-	return pagination.NewPagination(pq.Page, pq.PerPage, pr.Total), nil
-}
-
-func GetPaginationPgx(ctx context.Context, db *pgxpool.Conn, sb *sqlbuilder.SelectBuilder, pq pagination.PaginationQuery) (pagination.Pagination, error) {
-	paginationSQLStruct := sqlbuilder.NewStruct(new(paginationResponse))
-
-	sb.Select("count(*) as total").OrderBy().Limit(-1).Offset(-1)
-
-	query, args := sb.Build()
-
-	ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	rows, err := db.Query(ctxTimeout, query, args...)
 	if err != nil {
 		return pagination.Pagination{}, err
 	}
