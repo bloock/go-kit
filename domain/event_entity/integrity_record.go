86aace4913970d785bfde652d0632d6aae19369d
edutomesco <eduard.escoruela@gmail.com>
2024-01-16 17:17:34 +0000

    master | add client id to intrgrity record struct

--- domain/event_entity/integrity_record.go
+++ domain/event_entity/integrity_record.go
@@ -3,6 +3,7 @@ package event_entity
 type IntegrityRecord struct {
 	EventType    string            `json:"event_type"`
 	Record       string            `json:"record"`
+	ClientID     string            `json:"client_id"`
 	AnchorID     int               `json:"anchor_id"`
 	Test         bool              `json:"test"`
 	CreatedAt    int64             `json:"created_at"`
