7a400287fc37c9a6cf33314f251e83c6a5739d92
edutomesco <eduard.escoruela@gmail.com>
2024-02-28 12:48:54 +0000

    master | refactor free trial users

--- auth/resources.go
+++ auth/resources.go
@@ -5,6 +5,7 @@ type Resource string
 const (
 	ResourceUsersUser                 Resource = "users.user"
 	ResourceCredentialsApikey         Resource = "credentials.api_key"
+	ResourceCredentialsSession        Resource = "credentials.session"
 	ResourceCoreAnchor                Resource = "core.anchor"
 	ResourceCoreProof                 Resource = "core.proof"
 	ResourceCoreMessage               Resource = "core.message"
