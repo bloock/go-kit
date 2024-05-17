b9a12d047e0932948ad20f58b28e9f75229af5fe
edutomesco <eduard.escoruela@gmail.com>
2024-05-17 12:07:04 +0100

    master | add auth middleware

--- http/middleware/auth_middleware.go
+++ http/middleware/auth_middleware.go
@@ -1,46 +1,134 @@
 package middleware
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/bloock/go-kit/auth"
+	bloockCtx "github.com/bloock/go-kit/context"
 	httpError "github.com/bloock/go-kit/errors"
+	"github.com/bloock/go-kit/observability"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+	"io"
+	"net/http"
+	"time"
 )
 
-func AuthMiddleware(ability auth.Ability) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authorizationHeader := c.Request.Header.Get(auth.AUTHORIZATION_HEADER)
-		if authorizationHeader == "" {
-			_ = c.Error(httpError.ErrUnauthorized(errors.New("no authorization header found")))
-			c.Abort()
-			return
-		}
+type AuthMiddleware struct {
+	httpClient http.Client
+	authHost   string
+	logger     observability.Logger
+}
 
-		jwtToken := auth.GetBearerToken(authorizationHeader)
-		if jwtToken == "" {
-			_ = c.Error(httpError.ErrUnauthorized(errors.New("invalid token provided")))
-			c.Abort()
+func NewAuthMiddleware(authHost string, l observability.Logger) AuthMiddleware {
+	l.UpdateLogger(l.With().Caller().Str("component", "auth-middleware").Logger())
+
+	return AuthMiddleware{
+		httpClient: http.Client{},
+		authHost:   authHost,
+		logger:     l,
+	}
+}
+
+type CredentialAuthResponse struct {
+	JWT string `json:"jwt"`
+}
+
+type CredentialAuthErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (a AuthMiddleware) Authorize(ability auth.Ability) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		credAuthResp, err := a.getCredentialsAuthenticate(ctx)
+		if err != nil {
+			_ = ctx.Error(err)
+			ctx.Abort()
 			return
 		}
 
 		var claims auth.JWTClaims
-		err := auth.DecodeJWTUnverified(jwtToken, &claims)
+		err = auth.DecodeJWTUnverified(credAuthResp.JWT, &claims)
 		if err != nil {
-			_ = c.Error(httpError.ErrUnauthorized(errors.New("invalid token content provided")))
-			c.Abort()
+			_ = ctx.Error(httpError.ErrUnauthorized(errors.New("invalid token content provided")))
+			ctx.Abort()
 			return
 		}
+		ctx.Set(bloockCtx.UserIDKey, claims.ClientID)
 
 		if allowedActions := claims.Scopes[ability.Resource()]; allowedActions != nil {
 			for _, a := range allowedActions {
 				if a == ability.Action() {
-					c.Next()
+					ctx.Next()
 					return
 				}
 			}
 		}
 
-		_ = c.Error(httpError.ErrForbidden(errors.New("action forbidden")))
-		c.Abort()
+		_ = ctx.Error(httpError.ErrForbidden(errors.New("action forbidden")))
+		ctx.Abort()
 	}
+}
+
+func (a AuthMiddleware) getCredentialsAuthenticate(ctx *gin.Context) (CredentialAuthResponse, error) {
+	requestID, ok := ctx.Get(bloockCtx.RequestIDKey)
+	if !ok {
+		err := httpError.ErrUnexpected(errors.New("request id not found"))
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+
+	url := fmt.Sprintf("%s/credentials/v1/authenticate", a.authHost)
+
+	ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctxTimeout, http.MethodGet, url, nil)
+	if err != nil {
+		err = httpError.ErrUnexpected(err)
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+	req.Header = ctx.Request.Header
+	req.Header.Set(bloockCtx.RequestIDKey, requestID.(string))
+
+	resp, err := a.httpClient.Do(req)
+	if err != nil {
+		err = httpError.ErrUnexpected(err)
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	respByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		err = httpError.ErrUnexpected(err)
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		var errorMessage string
+		var respError CredentialAuthErrorResponse
+		err = json.Unmarshal(respByte, &respError)
+		if err != nil {
+			errorMessage = string(respByte)
+		}
+		errorMessage = respError.Message
+		err = httpError.NewHttpAppError(resp.StatusCode, errorMessage)
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+
+	var response CredentialAuthResponse
+	err = json.Unmarshal(respByte, &response)
+	if err != nil {
+		err = httpError.ErrUnexpected(err)
+		a.logger.Info(ctx).Err(err).Msg("")
+		return CredentialAuthResponse{}, err
+	}
+
+	return response, nil
 }
