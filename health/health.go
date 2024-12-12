5f900b847fa573385595f2eccdf3f4e39f37c648
edutomesco <eduard.escoruela@gmail.com>
2024-12-12 12:59:36 +0100

    master | adapt health handler to add gin chi

--- health/health.go
+++ health/health.go
@@ -2,6 +2,7 @@ package health
 
 import (
 	"fmt"
+	"github.com/bloock/go-kit/http/presenters"
 	"net/http"
 	"os"
 	"strings"
@@ -90,49 +91,70 @@ type HealthResponse struct {
 //		201: Health
 //		400:
 //		500:
-func (h Health) CheckHandler() gin.HandlerFunc {
+func (h Health) CheckGinHandler() gin.HandlerFunc {
 
 	return func(ctx *gin.Context) {
-		var details []ExternalServiceDetails
-		var err []string
-
-		serviceDown := false
-		for _, service := range h.services {
-			detail := service.HealthCheck()
-			details = append(details, detail)
-			if detail.Error != "" {
-				serviceDown = true
-				err = append(err, detail.Error)
-			}
-		}
-
-		var output string
-		fmt.Println()
-		if len(err) > 0 {
-			output = fmt.Sprintf("%s %s", h.output, strings.Join(err[:], ","))
-			ctx.JSON(http.StatusServiceUnavailable, HealthResponse{})
-			return
-		}
-
-		version := os.Getenv("IMAGE")
-		release := os.Getenv("IMAGE")
-
-		status := "pass"
-		if serviceDown {
-			status = "unestable"
-		}
-
-		health := HealthResponse{
-			Status:      status,
-			Version:     version,
-			RelaseID:    release,
-			Notes:       h.notes,
-			Output:      output,
-			Details:     details,
-			Links:       h.links,
-			ServiceID:   h.id,
-			Description: h.description,
-		}
-		ctx.JSON(http.StatusOK, health)
+		status, resp := h.getHealth()
+		ctx.JSON(status, resp)
 	}
+}
+
+// swagger:route GET /health infrastructure Health
+//
+//	Produces:
+//	- application/health+json
+//
+//	Schemes: http, https
+//
+//	Responses:
+//		201: Health
+//		400:
+//		500:
+func (h Health) CheckChiHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		status, resp := h.getHealth()
+		presenters.RenderJSON(w, r, status, resp)
+	}
+}
+
+func (h Health) getHealth() (int, interface{}) {
+	var details []ExternalServiceDetails
+	var err []string
+
+	serviceDown := false
+	for _, service := range h.services {
+		detail := service.HealthCheck()
+		details = append(details, detail)
+		if detail.Error != "" {
+			serviceDown = true
+			err = append(err, detail.Error)
+		}
+	}
+
+	var output string
+	if len(err) > 0 {
+		output = fmt.Sprintf("%s %s", h.output, strings.Join(err[:], ","))
+		return http.StatusServiceUnavailable, HealthResponse{}
+	}
+
+	version := os.Getenv("IMAGE")
+	release := os.Getenv("IMAGE")
+
+	status := "pass"
+	if serviceDown {
+		status = "unestable"
+	}
+
+	health := HealthResponse{
+		Status:      status,
+		Version:     version,
+		RelaseID:    release,
+		Notes:       h.notes,
+		Output:      output,
+		Details:     details,
+		Links:       h.links,
+		ServiceID:   h.id,
+		Description: h.description,
+	}
+	return http.StatusOK, health
 }
--- health/health_test.go
+++ health/health_test.go
@@ -26,7 +26,7 @@ func TestHandler_Check(t *testing.T) {
 	health := NewHealth("v0.0.1", "v0.0.1-release", "Error health check", nil, nil, nil)
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
-	r.GET("/health", health.CheckHandler())
+	r.GET("/health", health.CheckGinHandler())
 
 	t.Run("handler health returns 200", func(t *testing.T) {
 		req, err := http.NewRequest(http.MethodGet, "/health", nil)
@@ -43,7 +43,7 @@ func TestHandler_Check(t *testing.T) {
 	t.Run("handler health returns 500 when is not healthy", func(t *testing.T) {
 		r := gin.New()
 		health = NewHealth("v0.0.1", "v0.0.1-release", "Error health check", nil, nil, []ExternalService{NewFakeExternalService()})
-		r.GET("/health", health.CheckHandler())
+		r.GET("/health", health.CheckGinHandler())
 		req, err := http.NewRequest(http.MethodGet, "/health", nil)
 		require.NoError(t, err)
 		rec := httptest.NewRecorder()
